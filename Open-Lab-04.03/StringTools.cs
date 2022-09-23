5ded55ef13520e6db750a69d972e32618931c1f5
samuelmalik <samuelmalik9@gmail.com>
2022-09-23 20:58:27 +0200

    added function returning string without first and last character

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length > 1)
+            {
+                string first = original.Remove(0, 1);
+                string second = first.Remove(first.Length - 1);
+                return second;
+            }
+            else
+            {
+                return original;
+            }
+
         }
     }
 }
